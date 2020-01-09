549b29ee77ce171df54fe7479f5bdb2572c7aa7d
yijing <yijing5893@dingtalk.com>
2020-01-09 19:11:06 +0800

    创建路由

---
+++ music-app/src/routes/index.js
@@ -0,0 +1,16 @@
+import react from 'react'
+import { Redirect } from 'react-router-dom'
+import Home from '../application/Home'
+import Recommend from '../application/Recommend'
+import Singers from '../application/Singers'
+import Rank from '../application/Rank'
+
+export default [{
+    path: "/",
+    component: Home,
+    routes: [{
+        path: "/",
+        exact: true,
+    }]
+
+}]
