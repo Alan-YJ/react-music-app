8e20c4585bc5a26dca2a7b75624f5d03f6ddc994
yijing <734732794@qq.com>
2021-11-12 11:59:31 +0800

    develop提交1

--- music-app/src/App.js
+++ music-app/src/App.js
@@ -8,6 +8,7 @@ import { HashRouter } from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import './App.css';
 
+// develop 提交1
 function App() {
     return (
         <Provider store={store}>
